8df2b089c8caa51c5ceb19b6e984dd9fecad870e
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-07-31 09:20:55 +0530

    Create variable.tf

---
+++ Terraform/modules/variable.tf
@@ -0,0 +1,37 @@
+
+variable "ami_id" {
+  type        = string
+  description = "this ami id belongs to ap-south-1"
+
+}
+
+
+variable "instance_type" {
+  type = string
+
+}
+
+variable "key_name" {
+  type = string
+
+}
+
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+
+}
+
+
+variable "availability_zone" {
+  type = list(string)
+
+}
+
+variable "public_ip" {
+  type = bool
+}
